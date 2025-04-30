16f68b67c73b0d6cb4bff8f3b25bfcfd86a9a7e8
Bodi Ali <bodiali59@gmail.com>
2025-04-30 07:46:56 +0300

    Get value of context from parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useOutletContext } from "react-router";
 import "./App.css";
 
 export default function App() {
+  const { user } = useOutletContext();
+  console.log("App", user);
   return (
     <>
       <h1>Hello, World!</h1>
