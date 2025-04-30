98129ffccbb48cfe9f07b05c8c16434827d64a2e
Bodi Ali <bodiali59@gmail.com>
2025-04-30 08:51:07 +0300

    Feat: Create and style Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import logBlogIcon from "/images/the-log-blog-icon.svg";
+import { NavLink } from "react-router";
+import anonymousImage from "../../assets/images/anonymous.jpg";
+import styles from "./Header.module.css";
+
+export default function Header({ user, setToken }) {
+  function handleUserLogout() {
+    localStorage.removeItem("token");
+    setToken(localStorage.getItem("token"));
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.websiteLogo}>
+        <img src={logBlogIcon} alt="The Log Blog icon" />
+        <span>The Log Blog</span>
+      </div>
+      <nav className={styles.nav}>
+        <NavLink to="/" viewTransition>
+          Home
+        </NavLink>
+        {!user && (
+          <NavLink to="log-in" viewTransition>
+            Login
+          </NavLink>
+        )}
+        <a href="#">Manage your posts</a>
+      </nav>
+      {user && (
+        <div className={styles.profile}>
+          <p>
+            Welcome, {user.firstName} {user.lastName}
+          </p>
+          <img
+            src={user.Profile.profileImgUrl ? user.Profile.profileImgUrl : anonymousImage}
+            alt="User profile picture"
+          />
+          <button onClick={handleUserLogout}>Logout</button>
+        </div>
+      )}
+    </header>
+  );
+}
