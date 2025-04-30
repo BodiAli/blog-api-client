a40dcd83e7551a86190db97cec8c5ee73dcbb1a5
Bodi Ali <bodiali59@gmail.com>
2025-04-30 10:06:21 +0300

    Feat: Render about page in /about path && render NotFound component when an undefined path is entered

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,13 @@
 import UserProvider from "../utils/UserProvider";
 import App from "../App";
 import LoginPage from "../pages/LoginPage/LoginPage";
+import AboutPage from "../pages/AboutPage/AboutPage";
+import NotFound from "../pages/NotFoundPage/NotFound";
 
 const routes = [
   {
     Component: UserProvider,
+    ErrorBoundary: NotFound,
     children: [
       {
         path: "/",
@@ -17,6 +20,10 @@ const routes = [
           {
             path: "log-in",
             Component: LoginPage,
+          },
+          {
+            path: "about",
+            Component: AboutPage,
           },
         ],
       },
