ab5059788945d5db32413defcff99f0152923bbf
Bodi Ali <bodiali59@gmail.com>
2025-04-30 07:47:58 +0300

    Feat: Create routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,21 @@
+import UserProvider from "../utils/UserProvider";
+import App from "../App";
+import LoginPage from "../pages/LoginPage/LoginPage";
+
+const routes = [
+  {
+    Component: UserProvider,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "log-in",
+        Component: LoginPage,
+      },
+    ],
+  },
+];
+
+export default routes;
