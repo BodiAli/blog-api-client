7291f90025175a89873f0a2196c14465a1df6462
Bodi Ali <bodiali59@gmail.com>
2025-05-01 06:33:58 +0300

    Feat: Escape apostrophe

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -10,11 +10,12 @@ export default function AboutPage() {
       </div>
       <p className={styles.paragraph}>
         Welcome to The Log Blog, your all-in-one platform for sharing stories, sparking conversations, and
-        taking control of your online presence. Whether you’re a seasoned writer or just getting started, The
-        Log Blog makes it effortless to craft beautiful posts, engage with a vibrant community through
+        taking control of your online presence. Whether you&apos;re a seasoned writer or just getting started,
+        The Log Blog makes it effortless to craft beautiful posts, engage with a vibrant community through
         comments, and organize your content with our intuitive management tools. From drafting your first idea
         to tracking reader feedback, we give you everything you need to grow your voice and connect with
-        like-minded bloggers. Join us at The Log Blog—where your story isn’t just written, it’s celebrated.
+        like-minded bloggers. Join us at The Log Blog, where your story isn&apos;t just written, it&apos;s
+        celebrated.
       </p>
     </main>
   );
