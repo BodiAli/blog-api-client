001287462c803fa284d1851c28aaf8b14bad477e
Bodi Ali <bodiali59@gmail.com>
2025-05-02 10:23:35 +0300

    Feat: Handle liking post and only show like button to authenticated users

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,53 @@
+import { Link, useNavigate, useOutletContext } from "react-router";
 import { format } from "date-fns";
 import parse from "html-react-parser";
+import { toast } from "react-toastify";
 import anonymousImage from "../../assets/images/anonymous.jpg";
 import noImage from "../../assets/images/no-image.svg";
 import styles from "./Card.module.css";
 
-export default function Card({ post }) {
+export default function Card({ post, currentPage, topicQuery, updatePosts }) {
+  const { token, user } = useOutletContext();
+  const navigate = useNavigate();
+
+  async function handleLikePost(e) {
+    e.stopPropagation();
+
+    try {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${post.id}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      if (!res.ok) {
+        if (post.liked) {
+          throw new Error("Failed to remove like please try again later");
+        } else {
+          throw new Error("Failed to like post please try again later");
+        }
+      }
+
+      if (post.liked) {
+        toast.success("Like removed successfully!");
+      } else {
+        toast.success("Post liked successfully!");
+      }
+
+      updatePosts(currentPage, topicQuery);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   return (
-    <div className={styles.card}>
+    <div
+      onClick={() => {
+        navigate(`posts/${post.id}`, { viewTransition: true });
+      }}
+      className={styles.card}
+    >
       <div className={styles.text}>
         <div className={styles.textContainer1}>
           <img
@@ -37,6 +78,20 @@ export default function Card({ post }) {
             <strong>Likes: </strong>
             {post.likes}
           </p>
+          {!user ? (
+            <p className={styles.loginMessage}>
+              Please{" "}
+              <Link onClick={(e) => e.stopPropagation()} to="log-in" viewTransition>
+                Login
+              </Link>{" "}
+              to be able to like posts
+            </p>
+          ) : (
+            <button
+              onClick={handleLikePost}
+              className={`${styles.likeButton} ${post.liked ? styles.liked : ""}`}
+            ></button>
+          )}
         </div>
       </div>
       <div className={styles.imageContainer}>
