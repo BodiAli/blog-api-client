2711ab3265325c2f03a3d4506eb173b23d93f139
Bodi Ali <bodiali59@gmail.com>
2025-05-03 10:08:19 +0300

    Feat: Render comment data && handle liking comment and removing like from comment

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,28 +1,77 @@
+import { Link } from "react-router";
+import { toast } from "react-toastify";
+import { formatDistanceToNow } from "date-fns";
+import anonymousImage from "../../assets/images/anonymous.jpg";
 import styles from "./CommentCard.module.css";
 
-export default function CommentCard({ comment }) {
+export default function CommentCard({ comment, user, token, dispatch }) {
+  async function handleLikeComment() {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/posts/${comment.postId}/comments/${comment.id}/like`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      if (!res.ok) {
+        if (comment.commentLiked) {
+          throw new Error("Failed to remove like please try again later");
+        } else {
+          throw new Error("Failed to like comment please try again later");
+        }
+      }
+
+      if (comment.commentLiked) {
+        toast.success("Like removed successfully!");
+      } else {
+        toast.success("Comment liked successfully!");
+      }
+
+      dispatch({ type: "update-comment-like", comment });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   return (
     <div className={styles.card}>
-      <p>Comment</p>
+      <div className={styles.textContainer1}>
+        <img
+          src={comment.User.Profile.profileImgUrl ? comment.User.Profile.profileImgUrl : anonymousImage}
+          alt={`${comment.User.firstName} ${comment.User.lastName}'s profile picture`}
+        />
+        <p className={styles.name}>
+          {comment.User.firstName} {comment.User.lastName}
+        </p>
+        <p className={styles.createdAt}>
+          {formatDistanceToNow(comment.updatedAt, { addSuffix: true, includeSeconds: true })}
+        </p>
+      </div>
+      <div className={styles.textContainer2}>{comment.content}</div>
+      <div className={styles.likeContainer}>
+        <p className={styles.likes}>
+          <strong>Likes:</strong> {comment.likes}
+        </p>
+        {!user ? (
+          <p className={styles.loginMessage} data-testid="login-notice-comment">
+            Please{" "}
+            <Link to="log-in" viewTransition>
+              login
+            </Link>{" "}
+            to be able to like comment
+          </p>
+        ) : (
+          <button
+            data-testid="like-button-comment"
+            onClick={handleLikeComment}
+            className={`${styles.likeButton} ${comment.commentLiked ? styles.liked : ""}`}
+          ></button>
+        )}
+      </div>
     </div>
   );
 }
-
-// {
-//   "id": "0c17d514-68d9-4efa-af8b-c78da55bf88b",
-//   "userId": 1,
-//   "postId": "57c20dfb-8135-42eb-8abe-0bffbaffd668",
-//   "content": "Comment 11",
-//   "likes": 0,
-//   "createdAt": "2025-05-03T03:45:44.579Z",
-//   "updatedAt": "2025-05-03T03:45:44.579Z",
-//   "User": {
-//       "id": 1,
-//       "firstName": "Bodi",
-//       "lastName": "Ali",
-//       "Profile": {
-//           "profileImgUrl": "https://res.cloudinary.com/dgfglascb/image/upload/v1745107077/mrcja3i0xdylkidvxy3e.png"
-//       }
-//   },
-//   "commentLiked": false
-// }
