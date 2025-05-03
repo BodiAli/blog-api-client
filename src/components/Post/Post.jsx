2baaeb0dfc65892650dcc84e3d257cc9b39828c0
Bodi Ali <bodiali59@gmail.com>
2025-05-03 09:03:39 +0300

    Feat: Handle rendering comments && handle updating post like

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,22 +1,61 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router";
+import { useEffect, useReducer, useState } from "react";
+import { useNavigate, useParams, useOutletContext, Link } from "react-router";
 import { toast } from "react-toastify";
 import { format, formatDistanceToNow } from "date-fns";
 import parse from "html-react-parser";
+import CommentCard from "../CommentCard/CommentCard";
 import Loader from "../Loader/Loader";
 import noImage from "../../assets/images/no-image.svg";
 import styles from "./Post.module.css";
 
 export default function Post() {
-  const [post, setPost] = useState(null);
+  const [post, dispatch] = useReducer(reducer, null);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { postId } = useParams();
+  const { token, user } = useOutletContext();
+
+  async function handleLikePost(e) {
+    e.stopPropagation();
+
+    try {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${post.id}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      if (!res.ok) {
+        if (post.liked) {
+          throw new Error("Failed to remove like please try again later");
+        } else {
+          throw new Error("Failed to like post please try again later");
+        }
+      }
+
+      if (post.postLiked) {
+        toast.success("Like removed successfully!");
+      } else {
+        toast.success("Post liked successfully!");
+      }
+
+      dispatch({
+        type: "update-post-like",
+      });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
 
   useEffect(() => {
     async function fetchPost() {
       try {
-        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${postId}`);
+        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/${postId}`, {
+          headers: {
+            Authorization: token,
+          },
+        });
 
         if (!res.ok) {
           if (res.status === 404) {
@@ -28,8 +67,7 @@ export default function Post() {
 
         const fetchedPost = await res.json();
 
-        setPost(fetchedPost);
-        console.log(fetchedPost);
+        dispatch({ type: "set-post", post: fetchedPost });
       } catch (error) {
         toast.error(error.message);
         navigate(-1, { viewTransition: true });
@@ -38,7 +76,7 @@ export default function Post() {
       }
     }
     fetchPost();
-  }, [postId, navigate]);
+  }, [postId, navigate, token]);
 
   if (loading) return <Loader />;
 
@@ -56,12 +94,6 @@ export default function Post() {
         </p>
       </div>
       <div className={styles.textContainer2}>
-        <p className={styles.likes}>
-          <strong>Likes:</strong> {post?.likes}
-        </p>
-        <p className={styles.published}>{post?.published ? "Published" : "Unpublished"}</p>
-      </div>
-      <div className={styles.textContainer3}>
         <p>Topics:</p>
         <div>
           {post?.Topics.length === 0 ? (
@@ -77,8 +109,49 @@ export default function Post() {
         <img src={post?.imgUrl ? post?.imgUrl : noImage} alt="Post cover" />
         <div className={styles.postContentHtml}>{post && parse(post.content)}</div>
       </div>
-
-      <div className={styles.commentsContainer}></div>
+      <hr />
+      <div className={styles.likeContainer}>
+        <p className={styles.likes}>
+          <strong>Likes:</strong> {post?.likes}
+        </p>
+        {!user ? (
+          <p className={styles.loginMessage} data-testid="login-notice">
+            Please{" "}
+            <Link to="log-in" viewTransition>
+              login
+            </Link>{" "}
+            to be able to like posts
+          </p>
+        ) : (
+          <button
+            data-testid="like-button"
+            onClick={handleLikePost}
+            className={`${styles.likeButton} ${post.postLiked ? styles.liked : ""}`}
+          ></button>
+        )}
+      </div>
+      <div className={styles.commentsContainer}>
+        <h3>Comments</h3>
+        <div>
+          {post.Comments.map((comment) => {
+            return <CommentCard key={comment.id} comment={comment} />;
+          })}
+        </div>
+      </div>
     </main>
   );
+}
+
+function reducer(post, action) {
+  switch (action.type) {
+    case "set-post": {
+      return action.post;
+    }
+    case "update-post-like": {
+      return { ...post, postLiked: !post.postLiked, likes: post.postLiked ? post.likes - 1 : post.likes + 1 };
+    }
+    default: {
+      throw new Error(`Unknown action ${action.type}`);
+    }
+  }
 }
