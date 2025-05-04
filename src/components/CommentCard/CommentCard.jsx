f4cb4e336781e79827bcbd64f902e3209239ae5d
Bodi Ali <bodiali59@gmail.com>
2025-05-04 08:39:10 +0300

    Fix: Move dropdown inside container1 and rename textContainer classnames

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -3,6 +3,8 @@ import { toast } from "react-toastify";
 import { formatDistanceToNow } from "date-fns";
 import anonymousImage from "../../assets/images/anonymous.jpg";
 import optionsIcon from "../../assets/images/dots-vertical.svg";
+import editIcon from "../../assets/images/edit.svg";
+import deleteIcon from "../../assets/images/delete.svg";
 import styles from "./CommentCard.module.css";
 
 export default function CommentCard({ comment, user, token, dispatch }) {
@@ -40,34 +42,38 @@ export default function CommentCard({ comment, user, token, dispatch }) {
 
   return (
     <div className={styles.card}>
-      {comment.userId === user.id && (
-        <div className={styles.dropdown}>
-          <button className={styles.dropBtn}>
-            <img src={optionsIcon} alt="options" />
-          </button>
-          <div className={styles.dropdownContent}>
-            <div className={styles.folderEdit}>
-              <button>Edit</button>
-            </div>
-            <div className={styles.folderDelete}>
-              <button>Delete</button>
+      <div className={styles.container1}>
+        <div className={styles.commentUserInfo}>
+          <img
+            src={comment.User.Profile.profileImgUrl ? comment.User.Profile.profileImgUrl : anonymousImage}
+            alt={`${comment.User.firstName} ${comment.User.lastName}'s profile picture`}
+          />
+          <p className={styles.name}>
+            {comment.User.firstName} {comment.User.lastName}
+          </p>
+          <p className={styles.createdAt}>
+            {formatDistanceToNow(comment.updatedAt, { addSuffix: true, includeSeconds: true })}
+          </p>
+        </div>
+        {comment.userId === user.id && (
+          <div className={styles.dropdown}>
+            <button className={styles.dropBtn}>
+              <img src={optionsIcon} alt="options" />
+            </button>
+            <div className={styles.dropdownContent}>
+              <div className={styles.folderEdit}>
+                <img src={editIcon} alt="Edit comment" />
+                <button>Edit</button>
+              </div>
+              <div className={styles.folderDelete}>
+                <img src={deleteIcon} alt="delete comment" />
+                <button>Delete</button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      <div className={styles.textContainer1}>
-        <img
-          src={comment.User.Profile.profileImgUrl ? comment.User.Profile.profileImgUrl : anonymousImage}
-          alt={`${comment.User.firstName} ${comment.User.lastName}'s profile picture`}
-        />
-        <p className={styles.name}>
-          {comment.User.firstName} {comment.User.lastName}
-        </p>
-        <p className={styles.createdAt}>
-          {formatDistanceToNow(comment.updatedAt, { addSuffix: true, includeSeconds: true })}
-        </p>
+        )}
       </div>
-      <div className={styles.textContainer2}>{comment.content}</div>
+      <div className={styles.container2}>{comment.content}</div>
       <div className={styles.likeContainer}>
         <p className={styles.likes}>
           <strong>Likes:</strong> {comment.likes}
