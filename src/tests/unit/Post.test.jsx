513f6b8a92fe2045d6de031783e85d806d7d92cb
Bodi Ali <bodiali59@gmail.com>
2025-05-04 14:32:13 +0300

    Feat: Create tests for creating comments

--- src/tests/unit/Post.test.jsx
+++ src/tests/unit/Post.test.jsx
@@ -21,6 +21,7 @@ vi.mock("react-toastify", () => {
     toast: {
       error: vi.fn(),
       success: vi.fn(),
+      info: vi.fn(),
     },
   };
 });
@@ -194,6 +195,7 @@ describe("Post component", () => {
 
     expect(screen.getByTestId("likes-count")).toHaveTextContent("Likes: 3");
     expect(screen.getByTestId("like-button")).toHaveClass(/liked/i);
+    expect(toast.success).toHaveBeenCalledWith("Post liked successfully!");
   });
 
   test("should decrement post likes to 1 when clicking unlike button and remove liked class from like button", async () => {
@@ -222,9 +224,10 @@ describe("Post component", () => {
 
     expect(screen.getByTestId("likes-count")).toHaveTextContent("Likes: 1");
     expect(screen.getByTestId("like-button")).not.toHaveClass(/liked/i);
+    expect(toast.success).toHaveBeenCalledWith("Like removed successfully!");
   });
 
-  test("if fetch response is not ok because post is not found component should call navigate and error toast with expected arguments", async () => {
+  test("if fetch response is not ok because post is not found, component should call navigate and error toast with expected arguments", async () => {
     fetch.mockImplementation(() => {
       return Promise.resolve({
         ok: false,
@@ -252,4 +255,162 @@ describe("Post component", () => {
       "Post not found! it may have been moved, deleted or it might have never existed."
     );
   });
+
+  test("When create comment form is submitted and user is not authenticated it should info a toast", async () => {
+    const user = userEvent.setup();
+
+    useOutletContext.mockImplementation(() => ({ token: "", user: null }));
+
+    const Stub = createRoutesStub([
+      {
+        path: "/posts/:postId",
+        Component: () => <Post />,
+      },
+    ]);
+
+    await waitFor(() => {
+      render(<Stub initialEntries={["/posts/57c20dfb-8135-42eb-8abe-0bffbaffd668"]} />);
+    });
+
+    const commentInput = screen.getByPlaceholderText("Comment on post");
+    const createButton = screen.getByRole("button", { name: "Create" });
+
+    await user.type(commentInput, "Created comment");
+    await user.click(createButton);
+
+    expect(toast.info).toBeCalledWith("You need to login to create a comment");
+  });
+
+  test("When create comment form is submitted it should create a new comment", async () => {
+    const user = userEvent.setup();
+
+    // Implement two responses one for useEffect and one for handleCreateComment
+    fetch
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          json: () => Promise.resolve(fakePost),
+        });
+      })
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: true,
+          status: 201,
+          json: () =>
+            Promise.resolve({
+              comment: {
+                id: "1234-1234-1234-1234-1234",
+                userId: 1,
+                postId: "57c20dfb-8135-42eb-8abe-0bffbaffd668",
+                content: "Created comment",
+                likes: 1,
+                createdAt: "2025-05-03T07:51:49.273Z",
+                updatedAt: "2025-05-04T06:19:03.092Z",
+                User: {
+                  id: 1,
+                  firstName: "Bodi",
+                  lastName: "Ali",
+                  Profile: {
+                    profileImgUrl: "imageUrl",
+                  },
+                },
+                commentLiked: false,
+              },
+              msg: "Comment created successfully!",
+            }),
+        });
+      });
+
+    const Stub = createRoutesStub([
+      {
+        path: "/posts/:postId",
+        Component: () => <Post />,
+      },
+    ]);
+
+    await waitFor(() => {
+      render(<Stub initialEntries={["/posts/57c20dfb-8135-42eb-8abe-0bffbaffd668"]} />);
+    });
+
+    const commentInput = screen.getByPlaceholderText("Comment on post");
+    const createButton = screen.getByRole("button", { name: "Create" });
+
+    await user.type(commentInput, "Created comment");
+    await user.click(createButton);
+
+    expect(screen.getByText("Created comment")).toBeInTheDocument();
+    expect(toast.success).toHaveBeenCalledWith("Comment created successfully!");
+  });
+
+  test("If form is submitted with invalid input component should render invalid input error returned from server", async () => {
+    const user = userEvent.setup();
+
+    // Implement two responses one for useEffect and one for handleCreateComment
+    fetch
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          json: () => Promise.resolve(fakePost),
+        });
+      })
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: false,
+          status: 400,
+          json: () => Promise.resolve({ errors: [{ msg: "Comment can not be empty." }] }),
+        });
+      });
+
+    const Stub = createRoutesStub([
+      {
+        path: "/posts/:postId",
+        Component: () => <Post />,
+      },
+    ]);
+
+    await waitFor(() => {
+      render(<Stub initialEntries={["/posts/57c20dfb-8135-42eb-8abe-0bffbaffd668"]} />);
+    });
+
+    const commentInput = screen.getByPlaceholderText("Comment on post");
+    const createButton = screen.getByRole("button", { name: "Create" });
+
+    await user.type(commentInput, "Created comment");
+    await user.click(createButton);
+
+    expect(screen.getByRole("listitem")).toHaveTextContent("Comment can not be empty.");
+  });
+
+  // test("KOKO", async () => {
+  //   const user = userEvent.setup();
+
+  //   fetch
+  //     .mockImplementationOnce(() => {
+  //       return Promise.resolve({
+  //         ok: true,
+  //         status: 200,
+  //         json: () => Promise.resolve(fakePost),
+  //       });
+  //     })
+  //     .mockImplementationOnce(() => {
+  //       return Promise.resolve({
+  //         ok: false,
+  //         status: 400,
+  //         json: () => Promise.resolve({ errors: [{ msg: "Comment can not be empty." }] }),
+  //       });
+  //     });
+
+  //   const Stub = createRoutesStub([
+  //     {
+  //       path: "/posts/:postId",
+  //       Component: () => <Post />,
+  //     },
+  //   ]);
+
+  //   await waitFor(() => {
+  //     render(<Stub initialEntries={["/posts/57c20dfb-8135-42eb-8abe-0bffbaffd668"]} />);
+  //   });
+  // });
 });
